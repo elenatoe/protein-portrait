f74899ac93e46c9acda8e5ffd34dbf8ab9682f05
Zu Zu Lin Toe <elena9.ais@gmail.com>
2026-07-14 11:46:49 -0400

    Fix import statements and add main function check

--- src/protein_portrait/main.py
+++ src/protein_portrait/main.py
@@ -1,6 +1,10 @@
-from parser import read_fasta
-from properties import get_properties, sequence_to_properties
-from renderer import render_protein_portrait
+from .parser import read_fasta
+from .properties import get_properties, sequence_to_properties
+from .renderer import render_protein_portrait
+
+def main():
+    if _name_=="__main__":
+        main()
 protein_sequence = read_fasta("examples/xcl1.fasta")
 protein = list(protein_sequence.keys())[0]
 sequence = list(protein_sequence.values())[0]
